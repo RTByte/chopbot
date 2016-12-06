9eeb1741d4c3270aa3349e1d7bee530872434f1a
raaaasmus <raaaaaaasmus@gmail.com>
2016-12-06 23:27:58 +0100

    multiple commands and concepts

--- bot.js
+++ bot.js
@@ -8,10 +8,30 @@ client.on('ready',() => {
 
 var prefix = "-"
 client.on('message', message => {
-	if (message.author === client.user) return;
+  if (!message.content.startsWith(prefix)) return;
+  let args = message.content.split(' ').slice(1);
+	var result = args.join(' ');
+	if (message.author.bot) return;
+
+
 	if (message.content.startsWith(prefix + 'ping')) {
-		message.channel.sendMessage('Pong! `insert response time`');
-	}
+		message.channel.sendMessage(`Pong! \`${Date.now() - message.createdTimestamp} ms\``);
+	} else
+
+  if (message.content.startsWith(prefix + 'send')) {
+    client.channels.get('255819057616519169').sendMessage('different channel message concept (hardcoded)')
+  } else
+
+  if (message.content.startsWith(prefix + 'setgame')) {
+		if (!result) {
+			result = null;
+		}
+		client.user.setGame(result);
+  }
+
+  if (message.content.startsWith(prefix + 'foo')) {
+      message.channel.sendMessage('bar');
+  }
 });
 
 client.login(settings.token);
