e32659de82fd26d65b7bafa94245ed670d979a05
raaaasmus <raaaaaaasmus@gmail.com>
2017-01-16 00:19:22 +0100

    fuckin redid shit cause idk how to change profile pic otherwise

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const komada = require('komada');
 komada.start({
-  "botToken": "MjI2MDI3MTk2MzAzNjcxMjk2.Cyp09w.4a9ZcxbrlTgwl4yXOM371StwQLQ",
+  "botToken": "MjcwMzI5NjI1NjI2OTM1Mjk3.C12TwQ.18UEguD_WHNCTQOVJi0gOgPWzZU",
   "ownerid" : "106061111605878784",
-  "clientID": "226027196303671296",
+  "clientID": "270329625626935297",
   "prefix": "-",
   "clientOptions": {
     "fetchAllMembers": true
