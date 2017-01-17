8d7961f6b6d1a6bf10059299bf4568d38b5d1578
raaaasmus <raaaaaaasmus@gmail.com>
2017-01-18 00:00:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const komada = require('komada');
 komada.start({
-  "botToken": "MjcwMzI5NjI1NjI2OTM1Mjk3.C17WBA.Cv6Ci9ldZqpQClB2Ed2ruQWuDJU",
+  "botToken": "",
   "ownerid" : "106061111605878784",
   "clientID": "270329625626935297",
   "prefix": "-",
