f7bc78e3ee5cede3dd6b765c3843954c96a549b0
Uzui2012 <killian2100@hotmail.com>
2017-01-19 23:01:58 +0000

    Dont need reason to unmute

--- commands/Moderation/unmute.js
+++ commands/Moderation/unmute.js
@@ -17,6 +17,6 @@ exports.conf = {
 exports.help = {
   name: "unmute",
   description: "Unmutes mentioned discord user",
-  usage: "<user:user> <reason:str>",
-  usageDelim: " "
+  usage: "<user:user>",
+  usageDelim: ""
 };
