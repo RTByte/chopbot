57b70d176fa9e2993cf16b6a3f666a863ec93d27
raaaasmus <raaaaaaasmus@gmail.com>
2017-01-20 15:38:09 +0100

    Revert "Revert "Updated command logger""
    This reverts commit 52620dcc66c23a8d245eb61a46b4d472a5d1fa0e.

--- commands/General/choice.js
+++ commands/General/choice.js
@@ -15,8 +15,12 @@ exports.run = (client, msg, choices) => {
         icon_url: msg.guild.iconURL
       },
       color: 16645629,
-      title: "Command ran:",
-      description: `${msg.content}`,
+      fields: [{
+          name: "Command Content",
+          value: `\`${msg.content}\``,
+          inline: true
+        }
+      ],
       timestamp: new Date(),
       footer: {
         text: `${msg.author.username}#${msg.author.discriminator}`,
