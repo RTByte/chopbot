dcb041ded895417b94d31019f30608b2a7241108
raaaasmus <raaaaaaasmus@gmail.com>
2017-01-22 04:33:29 +0100

    fuck you

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const komada = require('komada');
 
 const client = komada.start({
-  "botToken": "MjcyMDE4Nzk5NzYxNDI0Mzg1.C2W1iQ.TbhC_lp6YMP6E2sAPyL7ObWlghA",
+  "botToken": "",
   "ownerid" : "106061111605878784",
   "clientID": "270329625626935297",
   "prefix": "-",
