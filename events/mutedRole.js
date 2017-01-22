3bbc5fa9eb1cd62b26ca71ffed5efdea090ebc0b
Uzui2012 <killian2100@hotmail.com>
2017-01-22 20:45:23 +0000

    Mute role creation apon entering server

---
+++ events/mutedRole.js
@@ -0,0 +1,21 @@
+exports.run = (client, guild) => {
+  let requestedRole = guild.roles.find("name", "Muted");
+  if(!requestedRole){
+    guild.createRole({ name: 'Muted',
+      hoist: false,
+      poistion: 0,
+      permissions:[0x400, 0x10000, 0x100000],
+      mentionable: false})
+    .then(role => console.log(`Created role ${role}`)
+    .catch(console.error);
+  }
+  else {
+    guild.roles.find("name", "Muted")
+    .edit({hoist: false,
+        poistion: 0,
+        permissions:[0x400, 0x10000, 0x100000],
+        mentionable: false})
+    .then(r => console.log(`Edited role ${r}`))
+    .catch(console.error));
+  }
+}
