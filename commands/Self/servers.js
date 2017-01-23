373e3e902bad2dbede689468fc70eede8e31e43f
raaaasmus <raaaaaaasmus@gmail.com>
2017-01-23 06:46:15 +0100

    new stuff

--- commands/Self/servers.js
+++ commands/Self/servers.js
@@ -5,7 +5,7 @@ exports.run = (client, msg) => {
     .setAuthor("RT Family Discord Servers", "http://i.imgur.com/ecCc6Ek.png")
     .setDescription("Click the name of the Discord server you want to join!")
     .addField("Let's Play Network", "• [Cow Chop Community](https://discord.gg/cowchop)\n• [Kinda Funny](https://discord.gg/kindafunny)\n• [The Creatures](https://discord.gg/d5YjjdP)\n• [Game Attack Community](https://discord.gg/F8fncjr)", true)
-    .addField("Rooster Teeth", "• [Rooster Teeth Community](https://https://discord.gg/roosterteeth)\n• [Funhaus Community](https://discord.gg/ecWNNZx)\n• [Achievement Hunter](https://discord.gg/P8cJ9vC)\n• [RTX 2017](https://discord.gg/0oqF8OqUW3gQDZD2)", true)
+    .addField("Rooster Teeth", "• [Rooster Teeth Community](https://https://discord.gg/roosterteeth)\n• [Funhaus Community](https://discord.gg/ecWNNZx)\n• [Achievement Hunter Community](https://discord.gg/P8cJ9vC)\n• [RTX 2017](https://discord.gg/0oqF8OqUW3gQDZD2)", true)
     .addField("Community Servers", "• [Community Bite](https://discord.gg/zQstVc9)", true)
     .addField("Rooster Teeth Shows", "• [/r/RWBY](https://discord.gg/rwby)\n• [Red vs. Blue](https://discord.gg/TkeEJ9D)", true)
     .setThumbnail("http://i.imgur.com/7lSighC.png", 50, 50)
