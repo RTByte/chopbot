b45883ba21e785e3f94c8fdca1fc0837676447b0
raaaasmus <raaaaaaasmus@gmail.com>
2017-01-24 04:58:01 +0100

    whitelisted users don't get deleted by blacklist

--- monitors/wordBlacklist.js
+++ monitors/wordBlacklist.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
-
 const profanityFinder = require("profanity-finder");
-
 const findProfanity = profanityFinder.findprofanity;
 
 exports.conf = {
@@ -11,23 +9,25 @@ exports.conf = {
 exports.run = (client, msg) => {
   return new Promise((resolve, reject) => {
     const bool = findProfanity(msg.content);
-    const whitelist = msg.guildConf.whiteList;
-    if (bool) {
-      msg.delete();
-
-      try {
-        const blacklistEmbed = new Discord.RichEmbed()
-          .setAuthor(`#${msg.channel.name}`, msg.guild.iconURL)
-          .setColor(16711680)
-          .addField("Blacklisted word detected. Message deleted.", `\`${msg.content}\``, true)
-          .setTimestamp()
-          .setFooter(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL)
-        client.channels.get(`${msg.guildConf.logChannel}`).sendEmbed(blacklistEmbed, '', { disableEveryone: true });
-      } catch (err) {
-        return;
-      }
+    if (msg.member.roles.has(msg.guildConf.whitelistedRole)) {
+      return;
     } else {
-      resolve();
+      if (bool) {
+        msg.delete();
+        try {
+          const blacklistEmbed = new Discord.RichEmbed()
+            .setAuthor(`#${msg.channel.name}`, msg.guild.iconURL)
+            .setColor(16711680)
+            .addField("Blacklisted word detected. Message deleted.", `\`${msg.content}\``, true)
+            .setTimestamp()
+            .setFooter(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL)
+          client.channels.get(`${msg.guildConf.logChannel}`).sendEmbed(blacklistEmbed, '', { disableEveryone: true });
+        } catch (err) {
+          return;
+        }
+      } else {
+        resolve();
+      }
     }
   });
 };
