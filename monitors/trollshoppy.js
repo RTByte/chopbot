6b6eee3ac4d4a40ca736dcc01d2718949e6abec5
raaaasmus <raaaaaaasmus@gmail.com>
2017-02-24 11:46:14 +0100

    commented out shoppy meme fully

--- monitors/trollshoppy.js
+++ monitors/trollshoppy.js
@@ -1,19 +1,19 @@
-let victim = ''; //'153887358133993474'; Shoppy's ID
-let reactEmoji = ''; //':shoppy:268174175229837312'; :shoppy: emoji on AHD's ID
-let ahdGuild = '' //'258447100193275904'; AHD's ID
+//let victim = ''; //'153887358133993474'; Shoppy's ID
+//let reactEmoji = ''; //':shoppy:268174175229837312'; :shoppy: emoji on AHD's ID
+//let ahdGuild = '' //'258447100193275904'; AHD's ID
 
-exports.conf = {
-    enabled: true,
-    spamProtection: false,
-}
+//exports.conf = {
+    //enabled: true,
+    //spamProtection: false,
+//}
 
-exports.run = (client, msg, guild) => {
-    return new Promise((resolve) => {
-        if (msg.guild.id === ahdGuild){
-            if (msg.author.id === victim){
-                msg.react(reactEmoji);
-            }
-        }
-        resolve();
-    });
-};
+//exports.run = (client, msg, guild) => {
+    //return new Promise((resolve) => {
+        //if (msg.guild.id === ahdGuild){
+            //if (msg.author.id === victim){
+                //msg.react(reactEmoji);
+            //}
+        //}
+        //resolve();
+    //});
+//};
