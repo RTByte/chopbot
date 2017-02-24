1e577a4e3195558ab50fefb9b34b4f23d57f5ae9
Michael Cumbers <me@michaelcumbers.ca>
2017-02-23 23:13:02 -0800

    Revert "Disabling shoppy meme to appease shoppy"
    This reverts commit a5eb6e3925f6ad24878e0a8c6c25280b0d72c2ae.

--- monitors/trollshoppy.js
+++ monitors/trollshoppy.js
@@ -1,6 +1,6 @@
-let victim = ''; //'153887358133993474'; Shoppy's ID
-let reactEmoji = ''; //':shoppy:268174175229837312'; :shoppy: emoji on AHD's ID
-let ahdGuild = '' //'258447100193275904'; AHD's ID
+let victim = '153887358133993474';
+let reactEmoji = ':shoppy:268174175229837312';
+let ahdGuild = '258447100193275904';
 
 exports.conf = {
     enabled: true,
