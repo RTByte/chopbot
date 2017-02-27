793ac0ff516987704119a5f5c1e683e858e4b669
raaaasmus <raaaaaaasmus@gmail.com>
2017-02-27 01:35:12 +0100

    update

--- monitors/wordBlacklist.js
+++ monitors/wordBlacklist.js
@@ -18,7 +18,7 @@ exports.run = (client, msg) => {
                     const blacklistEmbed = new Discord.RichEmbed()
                         .setAuthor(`#${msg.channel.name}`, msg.guild.iconURL)
                         .setColor("#ff0000")
-                        .addField("Blacklisted word detected. Message deleted.", `\`${msg.content}\``, true)
+                        .addField("Blacklisted word detected. Message deleted.", `${msg.content}`, true)
                         .setTimestamp()
                         .setFooter(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL)
                     client.channels.get(`${msg.guildConf.logChannel}`).sendEmbed(blacklistEmbed, '', {
