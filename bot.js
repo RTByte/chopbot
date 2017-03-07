e3ae2f5cfb368d8a8a75b1654aeb7b25ea7fb1b2
Michael Cumbers <me@michaelcumbers.ca>
2017-03-07 01:53:50 -0800

    rateWaifu command

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const komada = require('komada');
 
 const client = komada.start({
+    "botToken": "",
     "ownerid": "106061111605878784",
     "clientID": "270329625626935297",
     "prefix": "-",
