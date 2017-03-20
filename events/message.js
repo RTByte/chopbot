73c62f5dfd5556bb15b7473874332928082afca7
raaaasmus <raaaaaaasmus@gmail.com>
2017-03-20 23:58:06 +0100

    excluded #gitlab in ChopBot Dev from the dev mentions

--- events/message.js
+++ events/message.js
@@ -2,64 +2,66 @@ exports.run = (client, msg) => {
     const Discord = require('discord.js');
 
     // DEV MENTIONS BELOW
-    if (msg.content.toLowerCase().includes("rasmus")) {
-        try {
-            const rasMention = new Discord.RichEmbed()
-                .setAuthor(`${msg.guild.name}`, msg.guild.iconURL)
-                .setColor("#ffffff")
-                .addField(`Mention Detected (#${msg.channel.name})`, `${msg.content}`, true)
-                .setTimestamp()
-                .setFooter(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL);
-            client.channels.get(`283746370421260289`).sendEmbed(rasMention, '', {
-                disableEveryone: true
-            });
-        } catch (err) {
-            return;
-        }
-    }
-    if (msg.content.toLowerCase().includes("killian")) {
-        try {
-            const kilMention = new Discord.RichEmbed()
-                .setAuthor(`${msg.guild.name}`, msg.guild.iconURL)
-                .setColor("#ffffff")
-                .addField(`Mention Detected (#${msg.channel.name})`, `${msg.content}`, true)
-                .setTimestamp()
-                .setFooter(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL);
-            client.channels.get(`283746370421260289`).sendEmbed(kilMention, '', {
-                disableEveryone: true
-            });
-        } catch (err) {
-            return;
-        }
-    }
-    if (msg.content.toLowerCase().includes("sugar")) {
-        try {
-            const sugMention = new Discord.RichEmbed()
-                .setAuthor(`${msg.guild.name}`, msg.guild.iconURL)
-                .setColor("#ffffff")
-                .addField(`Mention Detected (#${msg.channel.name})`, `${msg.content}`, true)
-                .setTimestamp()
-                .setFooter(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL);
-            client.channels.get(`283746370421260289`).sendEmbed(sugMention, '', {
-                disableEveryone: true
-            });
-        } catch (err) {
-            return;
-        }
-    }
-    if (msg.content.toLowerCase().includes("stickman")) {
-        try {
-            const stickMention = new Discord.RichEmbed()
-                .setAuthor(`${msg.guild.name}`, msg.guild.iconURL)
-                .setColor("#ffffff")
-                .addField(`Mention Detected (#${msg.channel.name})`, `${msg.content}`, true)
-                .setTimestamp()
-                .setFooter(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL);
-            client.channels.get(`283746370421260289`).sendEmbed(stickMention, '', {
-                disableEveryone: true
-            });
-        } catch (err) {
-            return;
-        }
+    if (!msg.channel.id === 255839946093494273) {
+      if (msg.content.toLowerCase().includes("rasmus")) {
+          try {
+              const rasMention = new Discord.RichEmbed()
+                  .setAuthor(`${msg.guild.name}`, msg.guild.iconURL)
+                  .setColor("#ffffff")
+                  .addField(`Mention Detected (#${msg.channel.name})`, `${msg.content}`, true)
+                  .setTimestamp()
+                  .setFooter(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL);
+              client.channels.get(`283746370421260289`).sendEmbed(rasMention, '', {
+                  disableEveryone: true
+              });
+          } catch (err) {
+              return;
+          }
+      }
+      if (msg.content.toLowerCase().includes("killian")) {
+          try {
+              const kilMention = new Discord.RichEmbed()
+                  .setAuthor(`${msg.guild.name}`, msg.guild.iconURL)
+                  .setColor("#ffffff")
+                  .addField(`Mention Detected (#${msg.channel.name})`, `${msg.content}`, true)
+                  .setTimestamp()
+                  .setFooter(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL);
+              client.channels.get(`283746370421260289`).sendEmbed(kilMention, '', {
+                  disableEveryone: true
+              });
+          } catch (err) {
+              return;
+          }
+      }
+      if (msg.content.toLowerCase().includes("sugar")) {
+          try {
+              const sugMention = new Discord.RichEmbed()
+                  .setAuthor(`${msg.guild.name}`, msg.guild.iconURL)
+                  .setColor("#ffffff")
+                  .addField(`Mention Detected (#${msg.channel.name})`, `${msg.content}`, true)
+                  .setTimestamp()
+                  .setFooter(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL);
+              client.channels.get(`283746370421260289`).sendEmbed(sugMention, '', {
+                  disableEveryone: true
+              });
+          } catch (err) {
+              return;
+          }
+      }
+      if (msg.content.toLowerCase().includes("stickman")) {
+          try {
+              const stickMention = new Discord.RichEmbed()
+                  .setAuthor(`${msg.guild.name}`, msg.guild.iconURL)
+                  .setColor("#ffffff")
+                  .addField(`Mention Detected (#${msg.channel.name})`, `${msg.content}`, true)
+                  .setTimestamp()
+                  .setFooter(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL);
+              client.channels.get(`283746370421260289`).sendEmbed(stickMention, '', {
+                  disableEveryone: true
+              });
+          } catch (err) {
+              return;
+          }
+      }
     }
 };
