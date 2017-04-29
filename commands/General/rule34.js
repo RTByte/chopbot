5a64daaf092d332c4293f5b5396444c069e60cd4
Michael Cumbers <me@michaelcumbers.ca>
2017-04-28 18:36:26 -0700

    rule34 command

---
+++ commands/General/rule34.js
@@ -0,0 +1,26 @@
+exports.run = (client, msg, [...args]) => {
+    msg.reply(`No.`)
+    .then(reply => {
+        setTimeout(() => {
+            reply.delete();
+            msg.delete();
+        }, 5000);
+    })
+    .catch(console.error);
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    permLevel: 0,
+    botPerms: [],
+    requiredFuncs: []
+};
+
+exports.help = {
+    name: "rule34",
+    description: "Search for rule34",
+    usage: "[subject:str]",
+    usageDelim: " "
+};
