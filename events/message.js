6a13f1dee8f4bf3534f4f653f449cd8cc5135333
Michael Cumbers <me@michaelcumbers.ca>
2017-05-03 23:31:05 -0700

    Disabling Dev Logging

--- events/message.js
+++ events/message.js
@@ -1,10 +1,4 @@
 exports.run = (client, msg) => {
     const Discord = require('discord.js');
-
-    // Dev Mention Function
-    if (!(msg.channel.id === 255839946093494273)) {
-      if (msg.content.toLowerCase().includes("rasmus") || msg.content.toLowerCase().includes("killian") || msg.content.toLowerCase().includes("stickman")) {
-        client.funcs.devLog.devLog(client, msg, false);
-      }
-    }
+    //Dev Mention Disabled
 };
--- functions/devLog.js
+++ functions/devLog.js
@@ -2,18 +2,12 @@ const Discord = require('discord.js');
 
 exports.devLog = function(client, msg, isCommand = false) {
     if(client.devLogging){
-        const devLogger = new Discord.RichEmbed()
-            .setAuthor(`${msg.guild.name}`, msg.guild.iconURL)
-            .setColor("#ffffff")
-            .setTimestamp()
-            .setFooter(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL);
+        //Dev Logging Disabled
 
         if(isCommand){
-        	devLogger.addField("Command Content", `${msg.content}`, true);
-     		client.channels.get(client.devCommandLogChannel).sendEmbed(devLogger, '', {disableEveryone: true});
+        	//Dev Logging Disabled
         } else {
-        	devLogger.addField(`Mention Detected (#${msg.channel.name})`, `${msg.content}`, true);
-           	client.channels.get(client.devMentionChannel).sendEmbed(devLogger, '', {disableEveryone: true});
+        	//Dev Logging Disabled
         }
 
     }
