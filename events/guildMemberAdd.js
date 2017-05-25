e584b9292c259a30f647d15f41a3091a4c53bd1e
Michael Cumbers <me@michaelcumbers.ca>
2017-05-24 19:48:43 -0700

    Preliminary Work for User Caching

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,17 +3,52 @@ exports.run = (client, guildMember) => {
 
 	const guildConf = client.funcs.confs.get(guildMember.guild);
 
+    client.funcs.userCache.userExists(guildMember.user)
+    .then((userExists) => {
+        if(userExists){
+            client.funcs.userCache.serverExists(guildMember)
+            .then((serverExists) => {
+                if(serverExists){
+                    //User is re-joining this server
+                    userJoined(client, guildMember, Discord, guildConf, false);
+                } else {
+                    //User is joining this server for the first time
+                    client.funcs.userCache.newServer(guildMember)
+                    .then((err) => {
+                        userJoined(client, guildMember, Discord, guildConf, true);
+                    });
+                }
+            });
+        } else {
+            //User is new to both ChopBot and this server
+            client.funcs.userCache.newUser(guildMember)
+            .then((err) => {
+                client.funcs.userCache.newServer(guildMember)
+                .then((err) => {
+                    userJoined(client, guildMember, Discord, guildConf, true);
+                });
+            });
+        }
+    });
+
+};
+
+function userJoined(client, guildMember, Discord, guildConf, firstTime = true){
     try {
         const newUser = new Discord.RichEmbed()
             .setAuthor(`${guildMember.user.username}#${guildMember.user.discriminator} (${guildMember.user.id})`, guildMember.user.avatarURL)
             .setColor("#00ff00")
             .setTimestamp()
             .setFooter(`User joined`);
+        
+        if(!firstTime){
+            newUser.setFooter(`User re-joined`);
+        }
+
         client.channels.get(`${guildConf.logChannel}`).sendEmbed(newUser, '', {
             disableEveryone: true
         });
     } catch (err) {
         return;
     }
-
-};
+}
--- events/message.js
+++ events/message.js
@@ -1,4 +1,61 @@
 exports.run = (client, msg) => {
     const Discord = require('discord.js');
-    //Dev Mention Disabled
+    
+    //Here there be dragons...
+
+    if(msg.guild){
+	    //Checking to see if this user exists
+	    client.funcs.userCache.userExists(msg.author)
+	    .then((userExists) => {
+	    	msg.guild.fetchMember(msg.author).then((guildMember) => {
+	    		if(userExists){
+	    			//Checking to see if user has this server tracked
+	    			client.funcs.userCache.serverExists(guildMember)
+	    			.then((serverTracked) => {
+	    				if(serverTracked){
+	    					//Performing usual duties
+	    					addXP(client, msg, guildMember);
+	    				} else {
+	    					//Registering this server for the user
+	    					client.funcs.userCache.newServer(guildMember)
+	    					.then((err) => {
+								addXP(client, msg, guildMember);
+	    					});
+	    				}
+	    			});
+		
+	    		} else {
+	    			//Registering this user
+	    			client.funcs.userCache.newUser(guildMember)
+	    			.then((err) => {
+	    				//Registering this server for the user
+	    				client.funcs.userCache.newServer(guildMember)
+	    				.then((err) => {
+	    					//Performing usual duties
+	    					addXP(client, msg, guildMember);
+	    				});
+	    			});
+	    		}
+	    	});
+
+
+	    });
+    }
+
 };
+
+function addXP(client, msg, guildMember){
+	//Performing usual duties
+	client.funcs.userCache.addXP(guildMember, msg)
+	.then((earnedXP) => {
+		client.funcs.userCache.updateLevel(guildMember, msg)
+		.then((levelUp) => {
+			if(levelUp){
+			 //Notify of Level Up? TODO
+			}
+			    					
+			client.funcs.userCache.addMessage(guildMember, msg)
+			.then((globalMessageCount) => {});
+		});
+	});
+}
