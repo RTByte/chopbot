b00c2e2a689dcec553da0fbb17b25db59bd88d04
Rasmus Gerdin <me@rasmusgerdin.com>
2018-02-13 01:01:01 +0100

    Added hostinfo command

---
+++ commands/Developer/hostinfo.js
@@ -0,0 +1,34 @@
+exports.run = async (client, msg) => {
+    const os = require('os');
+
+    const hostinfoEmbed = new client.methods.Embed()
+      .setAuthor(target.username, target.avatarURL())
+      .setColor("#ffffff")
+      .addField("Hostname", os.hostname(), true)
+      .addField("Running on", os.type(), true)
+      .addField("CPU Architecture", os.arch(), true)
+      .addField("Uptime", os.uptime(), true)
+      .addField("Load Average", os.loadavg(), true)
+
+      .setThumbnail("http://i.imgur.com/7lSighC.png", 50, 50)
+      .setTimestamp()
+      .setFooter(`Requested by ${msg.author.tag}`, msg.author.avatarURL());
+    return msg.channel.send('', { disableEveryone: true, embed: hostinfoEmbed });
+};
+
+exports.conf = {
+    enabled: true,
+    runIn: ["text"],
+    aliases: ["hinfo"],
+    permLevel: 10,
+    botPerms: [],
+    requiredFuncs: [],
+};
+
+exports.help = {
+    name: "hostinfo",
+    description: "Shows info about bot host.",
+    usage: "",
+    usageDelim: "",
+    type: "debug",
+};
