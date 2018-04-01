42687e57f9e25bae3b7cbb0f659bca73d2668e5a
Rasmus Gerdin <me@rasmusgerdin.com>
2018-04-02 00:44:09 +0200

    Fix moonball command, emoji broke

--- commands/Everyone/moonball.js
+++ commands/Everyone/moonball.js
@@ -14,7 +14,7 @@ exports.run = async (client, msg) => {
   	if (game && (await client.funcs.moonball.gameOver(client, guildMember)) >= 0) {
   		await client.funcs.moonball.forceEndGame(client, guildMember);
   		game = false;
-  	} 
+  	}
 
   	//If there is no game, we start a new one
   	if (!game) {
@@ -36,8 +36,8 @@ exports.run = async (client, msg) => {
 	}
 
 	//Sending the updated info to the channel
-	await msg.channel.send(`ðŸ€<@${gameState.thrower}> throws the ball to <@${gameState.catcher}>!\nCurrent Reward: **${gameState.reward}**xp`);
-  
+	await msg.channel.send(`🏀 <@${gameState.thrower}> throws the ball to <@${gameState.catcher}>!\nCurrent Reward: **${gameState.reward}**xp`);
+
   	return (await setTimeout(async () => {
     	const endGame = await client.funcs.moonball.endGame(client, guildMember);
 
